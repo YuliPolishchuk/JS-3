bfab9d5fb802d85d5d41cbbc27fd8d5b7d313b9f
YuliPolishchuk <44016256+YuliPolishchuk@users.noreply.github.com>
2018-12-21 09:52:33 +0200

    Update scriptGetArr.js

--- js/scriptGetArr.js
+++ js/scriptGetArr.js
@@ -1,6 +1,6 @@
 var arr = [];
 function getMaxSumSub(arr) {
-  var max = 0;
+  var max = -Infinity;
   var sum = 0;
   for(var i = 0; i < arr.length; i++) {
     for(var j = i; j < arr.length; j++) {
@@ -13,7 +13,8 @@ function getMaxSumSub(arr) {
     }
     sum = 0;
   }
-  return (max > 0) ? max : -1;
+  return (max > 0) ? max: -1;
 }
 console.log(getMaxSumSub([1, 34, -43, 24, 42, 2]));
 console.log(getMaxSumSub([1, -2, 3, 4, -9, 6]));
+console.log(getMaxSumSub([-1, -2, -3, 4, -9, -6]));
