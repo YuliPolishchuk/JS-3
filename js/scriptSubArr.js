0d86bb075f88628ca7001ff527efef22a2f4a18e
YuliPolishchuk <44016256+YuliPolishchuk@users.noreply.github.com>
2018-12-22 20:18:45 +0200

    Update scriptSubArr.js

--- js/scriptSubArr.js
+++ js/scriptSubArr.js
@@ -1,13 +1,14 @@
 var arr = [1, 2, 3, 4];
 var arrSub = [];
 function getSubArr(array) {
-  for(i = 0; i < array.length; i++) {
-    for(var j = i; j < array.length; j++) {
-      arrSub.push(array.slice(i, j+1));
+  for (i = 0; i < array.length; i++) {
+    for (var j = i; j < array.length; j++) {
+      arrSub.push(array.slice(i, j + 1));
     }
   }
   return arrSub;
 }
 console.log(getSubArr(arr));
+
 
 
