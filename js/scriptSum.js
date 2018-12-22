5c144844f68f1dbefab2cd1b9164784f66f43932
YuliPolishchuk <44016256+YuliPolishchuk@users.noreply.github.com>
2018-12-22 20:19:07 +0200

    Update scriptSum.js

--- js/scriptSum.js
+++ js/scriptSum.js
@@ -1,7 +1,7 @@
 var arr = [3, 2, 5, 6, 24, 34, 34, 44];
 function arraySum(array) {
   var sum = 0;
-  for(var i = 0; i < array.length; i++) {
+  for (var i = 0; i < array.length; i++) {
     sum += array[i];
   }
   console.log(sum);
