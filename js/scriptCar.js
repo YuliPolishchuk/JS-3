1ffd62ffd1187466317f0dacab8fe39d9a37f80d
YuliPolishchuk <44016256+YuliPolishchuk@users.noreply.github.com>
2018-12-24 14:35:37 +0200

    Update scriptCar.js

--- js/scriptCar.js
+++ js/scriptCar.js
@@ -6,40 +6,40 @@
   ignition: false,
   amountPetrol: 7,
 
-isIgnition: function() {
-  this.ignition = true;
-},
+  isIgnition: function() {
+    this.ignition = true;
+  },
 
-start: function() {
-  if (this.ignition) {
-    console.log('Машина ' + this.brand + ' цвета - ' + this.color + ' поехала');
-  } else {
-    console.log('Включите вначале зажигание');
-  }
-},
+  start: function() {
+    if (this.ignition) {
+      console.log('Машина ' + this.brand + ' цвета - ' + this.color + ' поехала');
+    } else {
+      console.log('Включите вначале зажигание');
+    }
+  },
 
-stop: function() {
-  if (this.ignition) {
-    this.ignition = false;
-    console.log('Машинка остановилась');
-  } else {
-    console.log('Зажигание и так выключено');
-  }
-},
+  stop: function() {
+    if (this.ignition) {
+      this.ignition = false;
+      console.log('Машинка остановилась');
+    } else {
+      console.log('Зажигание и так выключено');
+    }
+  },
 
-isPetrol: function() {
-  if (this.amountPetrol > 0) {
-    console.log(this.ignition);
-  } else {
-    console.log('Нужно заправить автомобиль');
-  }
-},
+  isPetrol: function() {
+    if (this.amountPetrol > 0) {
+      console.log(this.ignition);
+    } else {
+      console.log('Нужно заправить автомобиль');
+    }
+  },
 
-toTankUpCar: function() {
-  this.amountPetrol = 10;
-  console.log('Машинка заправилась')
-  }
-};
+  toTankUpCar: function() {
+    this.amountPetrol = 10;
+    console.log('Машинка заправилась')
+    }
+  };
  
  car.isIgnition();
  car.start();
